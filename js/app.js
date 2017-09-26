9151db4f68f29e80f1f415b14219167f74cc757f
mfullford <marissafullford@gmail.com>
2017-09-25 18:05:21 -0600

    Adds winner

--- js/app.js
+++ js/app.js
@@ -14,29 +14,33 @@ $(document).ready(function() {
 		count += 1;
 		if (count %2 == 0) {
 			$(this).append("X").addClass("X");
+			isWinner();
 		} else {
 			$(this).append('O').addClass("X");
+			isWinner();
 		}
 		 $(this).off('click');
-	}
+	
 
 	function isWinner() {
-	if  (
-		($("#box1").hasClass("X") && $("#box2").hasClass("X") && $("#box3").hasClass("X")) ||
-		($("#box1").hasClass( "X" ) && $("#box4").hasClass( "X" ) && $("#box7").hasClass( "X" )) ||
-		($("#box1").hasClass( "X" ) && $("#box5").hasClass( "X" ) && $("#box9").hasClass( "X" )) ||
-		($("#box2").hasClass( "X" ) && $("#box5").hasClass( "X" ) && $("#box8").hasClass( "X" )) ||
-		($("#box3").hasClass( "X" ) && $("#box6").hasClass( "X" ) && $("#box9").hasClass( "X" )))
-		{return alert("Yas queen! That does impress me much! Player 2 is the winner.");}
-	else if (
-		($("#box1").hasClass("O") && $("#box2").hasClass("O") && $("#box3").hasClass("O")) ||
-		($("#box1").hasClass("O") &&  $("#box4").hasClass("O") && $("#box7").hasClass("O")))
-		{return alert("Yas queen! That does impress me much! Player 1 is the winner.");}
-	};
-
+		if  (
+			($("#box1").hasClass("X") && $("#box2").hasClass("X") && $("#box3").hasClass("X")) ||
+			($("#box1").hasClass( "X" ) && $("#box4").hasClass( "X" ) && $("#box7").hasClass( "X" )) ||
+			($("#box1").hasClass( "X" ) && $("#box5").hasClass( "X" ) && $("#box9").hasClass( "X" )) ||
+			($("#box2").hasClass( "X" ) && $("#box5").hasClass( "X" ) && $("#box8").hasClass( "X" )) ||
+			($("#box3").hasClass( "X" ) && $("#box6").hasClass( "X" ) && $("#box9").hasClass( "X" )))
+			{return  alert("Yas queen! That does impress me much! Player 2 is the winner.");}
+		else if (
+			($("#box1").hasClass("O") && $("#box2").hasClass("O") && $("#box3").hasClass("O")) ||
+			($("#box1").hasClass("O") &&  $("#box4").hasClass("O") && $("#box7").hasClass("O")))
+			{return alert("Yas queen! That does impress me much! Player 1 is the winner.");}
+		else {}
+	}
+};
 
 	$('#finished').click(function() {
     location.reload();
 	});
 });
+
 
